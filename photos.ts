34a5ba4c2c56ff536c8dca84efdf1925d55f6507
JHNLWHD <aljhoenilw@gmail.com>
2025-01-18 16:31:55 +0800

    feat: implement initial code for photo gallery using react-photo-album package

---
+++ photos.ts
@@ -0,0 +1,31 @@
+import type { Photo } from "react-photo-album";
+
+const breakpoints = [1080, 640, 384, 256, 128, 96, 64, 48];
+
+function assetLink(asset: string, width: number) {
+    return `https://jhoejie-wedding-photo-album.vercel.app/_next/image?url=${encodeURIComponent(`/images/${asset}`)}&w=${width}&q=75`;
+}
+
+const photos = [
+  {
+    asset: "intro.jpg",
+    width: 1080,
+    height: 780,
+    alt: "JhoeJie Picture with Newspaper Invitation",
+  },
+].map(
+  ({ asset, alt, width, height }) =>
+    ({
+      src: assetLink(asset, width),
+      alt,
+      width,
+      height,
+      srcSet: breakpoints.map((breakpoint) => ({
+        src: assetLink(asset, breakpoint),
+        width: breakpoint,
+        height: Math.round((height / width) * breakpoint),
+      })),
+    }) as Photo,
+);
+
+export default photos;
